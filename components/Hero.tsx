3e690ad81b99d3f06734723eb3082afa534bcb0d
shinehtetnaing <shinehtetnaing1998@gmail.com>
2023-09-13 20:57:48 +0630

    searchbar for manufacturer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import { CustomButton } from "."
 
 const Hero = () => {
-  const handleScroll = () => {}
+  const handleScroll = () => { }
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -14,7 +14,7 @@ const Hero = () => {
 
         <p className="hero__subtitle">Streamline your car rental experience with our effortless booking process.</p>
 
-        <CustomButton 
+        <CustomButton
           title="Explore Cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
@@ -22,7 +22,7 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/hero.png" alt="hero" fill className="object-contain"/>
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
 
         <div className="hero__image-overlay" />
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,14 +5,16 @@ import { useState } from "react"
 import { SearchManufacturer } from "."
 
 const SearchBar = () => {
-  const [manufacturer, setManufacturer] = useState([])
+  const [manufacturer, setManufacturer] = useState('')
 
-  const handleSearch = () => {}
+  const handleSearch = () => { }
+
   return (
-    <form className="searchbar"  onSubmit={handleSearch}>
+    <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
         <SearchManufacturer
-        
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
         />
       </div>
     </form>
